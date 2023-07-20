fdd2ff1d951dd28f349b9cbbc53f8f7cc2747360
olekw <olekw@NSNL>
2023-07-20 16:54:08 +0200

    Add project files.

---
+++ blackjack/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using blackjack;
+using Microsoft.VisualBasic;
+using System;
+/*
+var cardProperties = new List<cardProperties> {
+new cardProperties() { power=1, name="1"},
+new cardProperties() { power=2, name="2"}
+};
+*/
+string[] name = new string[3];
+name[0] = "King";
+name[1] = "Queen";
+name[2] = "Jack";
+Random rnd = new Random();
+Console.WriteLine(name[rnd.Next(0, 2)]);
+var result = Console.ReadLine(name[rnd.Next(0, 2)]);
---
+++ blackjack/cardProperties.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace blackjack
+{
+    public class cardProperties
+    {
+        public int power { get; set; } 
+        public string name { get; set; }
+
+
+
+
+    }
+}
