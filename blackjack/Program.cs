1514f533aca8480d33622bb5a5162414e4311f4f
user <olekwasowskipl@gmail.com>
2023-07-25 21:15:20 +0200

    progress

--- blackjack/Program.cs
+++ blackjack/Program.cs
@@ -26,40 +26,48 @@ name[2] = "4";
 name[1] = "3";
 name[0] = "2";
 ;
+List<cards> ListofCards = new List<cards>();
+int totalValue = 0;
 Random rnd = new Random();
 var firstCard = name[rnd.Next(0, 11)];
-if (firstCard == name[0]) { cards card = new card() { power = 1, name = "2" }; }/*
-else if (firstCard == name[1]) { int ValueofFirst = 3; }
-else if (firstCard == name[2]) { int ValueofFirst = 4; }
-else if (firstCard == name[3]) { int ValueofFirst = 5; }
-else if (firstCard == name[4]) { int ValueofFirst = 6; }
-else if (firstCard == name[5]) { int ValueofFirst = 7; }
-else if (firstCard == name[6]) { int ValueofFirst = 8; }
-else if (firstCard == name[7]) { int ValueofFirst = 9; }
-else if (firstCard == name[8]) { int ValueofFirst = 10; }
-else if (firstCard == name[9]) { int ValueofFirst = 10; }
-else if (firstCard == name[10]) { int ValueofFirst = 10; }
-else if (firstCard == name[11]) { int ValueofFirst = 10; }
+if (firstCard == name[0])
+{
+    totalValue += 2;
+}
+else if (firstCard == name[0])
+{
+    totalValue += 3;
+}
+else if (firstCard == name[2]) {     totalValue += 4; }
+else if (firstCard == name[3]) {     totalValue += 5; }
+else if (firstCard == name[4]) {     totalValue += 6; }
+else if (firstCard == name[5]) {     totalValue += 7; }
+else if (firstCard == name[6]) {     totalValue += 8; }
+else if (firstCard == name[7]) {     totalValue += 9; }
+else if (firstCard == name[8]) {     totalValue += 10; }
+else if (firstCard == name[9]) {     totalValue += 10; }
+else if (firstCard == name[10]){     totalValue += 10; }
+else if (firstCard == name[11]) {    totalValue += 10;  }
 
 Console.WriteLine(firstCard);
 var secondCard = name[rnd.Next(0, 11)];
 
-if (firstCard == name[0]) {  int ValueofSecond = 2; }
-else if (secondCard == name[1]) { int ValueofSecond = 3; }
-else if (secondCard == name[2]) { int ValueofSecond = 4; }
-else if (secondCard == name[3]) { int ValueofSecond = 5; }
-else if (secondCard == name[4]) { int ValueofSecond = 6; }
-else if (secondCard == name[5]) { int ValueofSecond = 7; }
-else if (secondCard == name[6]) { int ValueofSecond = 8; }
-else if (secondCard == name[7]) { int ValueofSecond = 9; }
-else if (secondCard == name[8]) { int ValueofSecond = 10; }
-else if (secondCard == name[9]) { int ValueofSecond = 10; }
-else if (secondCard == name[10]) { int ValueofSecond = 10; }
-else if (secondCard == name[11]) { int ValueofSecond = 10; }
-*/
+if (secondCard == name[0]) { totalValue += 2; }
+else if (secondCard == name[1]) { totalValue += 3; }
+else if (secondCard == name[2]) { totalValue += 4; }
+else if (secondCard == name[3]) { totalValue += 5; }
+else if (secondCard == name[4]) { totalValue += 6; }
+else if (secondCard == name[5]) { totalValue += 7; }
+else if (secondCard == name[6]) { totalValue += 8; }
+else if (secondCard == name[7]) { totalValue += 9; }
+else if (secondCard == name[8]) { totalValue += 10; }
+else if (secondCard == name[9]) { totalValue += 10; }
+else if (secondCard == name[10]){ totalValue += 10; }
+else if (secondCard == name[11]){ totalValue += 10; }
+
 Console.WriteLine(secondCard);
-// tu jest błąd, jakby nie widziało zmiennej
-int SumofValues = (ValueofFirst + ValueofSecond);
+
+int SumofValues = (totalValue);
 Console.WriteLine(SumofValues + " value");
 Console.WriteLine("do you want to hit(1) or stand(2)?");
 int choice = int.Parse(Console.ReadLine());
