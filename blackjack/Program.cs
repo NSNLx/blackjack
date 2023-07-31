648242212ac8e16bc8892af6984e2dc7e31d5aeb
NSNLx <olekwasowskipl@gmail.com>
2023-07-31 09:21:58 +0200

    progress

--- blackjack/Program.cs
+++ blackjack/Program.cs
@@ -8,9 +8,10 @@ new cardProperties() { power=1, name="1"},
 new cardProperties() { power=2, name="2"}
 };
 */
-
-
-
+Random rndDealer = new Random();
+var Dealer = rndDealer.Next(12, 22);
+Console.WriteLine($"Dealer has {Dealer}");
+if (Dealer > 21) { Console.Write(" Dealer has busted, you won."); }
 
 string[] name = new string[12];
 name[11] = "King";
@@ -26,30 +27,45 @@ name[2] = "4";
 name[1] = "3";
 name[0] = "2";
 ;
-List<cards> ListofCards = new List<cards>();
+//List<cards> ListofCards = new List<cards>();
 int totalValue = 0;
 Random rnd = new Random();
-var firstCard = name[rnd.Next(0, 11)];
-if (firstCard == name[0])
-{
-    totalValue += 2;
-}
-else if (firstCard == name[0])
-{
-    totalValue += 3;
-}
-else if (firstCard == name[2]) {     totalValue += 4; }
-else if (firstCard == name[3]) {     totalValue += 5; }
-else if (firstCard == name[4]) {     totalValue += 6; }
-else if (firstCard == name[5]) {     totalValue += 7; }
-else if (firstCard == name[6]) {     totalValue += 8; }
-else if (firstCard == name[7]) {     totalValue += 9; }
-else if (firstCard == name[8]) {     totalValue += 10; }
-else if (firstCard == name[9]) {     totalValue += 10; }
-else if (firstCard == name[10]){     totalValue += 10; }
-else if (firstCard == name[11]) {    totalValue += 10;  }
 
-Console.WriteLine(firstCard);
+while (true)
+{
+    var Card = name[rnd.Next(0, 11)];
+    if (Card == name[0]) {totalValue += 2;}
+    else if (Card == name[0]) {totalValue += 3;}
+    else if (Card == name[2]) { totalValue += 4; }
+    else if (Card == name[3]) { totalValue += 5; }
+    else if (Card == name[4]) { totalValue += 6; }
+    else if (Card == name[5]) { totalValue += 7; }
+    else if (Card == name[6]) { totalValue += 8; }
+    else if (Card == name[7]) { totalValue += 9; }
+    else if (Card == name[8]) { totalValue += 10; }
+    else if (Card == name[9]) { totalValue += 10; }
+    else if (Card == name[10]) { totalValue += 10; }
+    else if (Card == name[11]) { totalValue += 10; }
+
+    Console.WriteLine(Card);
+    int SumofValues = (totalValue);
+    Console.WriteLine(SumofValues + " value");
+    if (SumofValues > 21) { Console.WriteLine("It is a bust"); break; }
+    Console.WriteLine("do you want to hit(1) or stand(2)?");
+    int choice=Int32.Parse(Console.ReadLine());
+    if (choice == 2)
+    {
+        if (SumofValues > Dealer) { Console.WriteLine("You won!"); }
+        else if (SumofValues < Dealer) { Console.WriteLine("You lost!"); }
+        else if(SumofValues == Dealer) { Console.WriteLine("It is a push, no one won,"); }
+            
+            break; }  
+    
+
+}
+
+
+/*
 var secondCard = name[rnd.Next(0, 11)];
 
 if (secondCard == name[0]) { totalValue += 2; }
@@ -62,8 +78,8 @@ else if (secondCard == name[6]) { totalValue += 8; }
 else if (secondCard == name[7]) { totalValue += 9; }
 else if (secondCard == name[8]) { totalValue += 10; }
 else if (secondCard == name[9]) { totalValue += 10; }
-else if (secondCard == name[10]){ totalValue += 10; }
-else if (secondCard == name[11]){ totalValue += 10; }
+else if (secondCard == name[10]) { totalValue += 10; }
+else if (secondCard == name[11]) { totalValue += 10; }
 
 Console.WriteLine(secondCard);
 
@@ -72,9 +88,4 @@ Console.WriteLine(SumofValues + " value");
 Console.WriteLine("do you want to hit(1) or stand(2)?");
 int choice = int.Parse(Console.ReadLine());
 
-if (choice == 1)
-{
-    var thirdCard = name[rnd.Next(0, 11)];
-    Console.WriteLine(thirdCard);
-}
-
+*/
