056fcb724532035e801f105c39e1e42790faf216
NSNLx <olekwasowskipl@gmail.com>
2023-08-01 20:29:21 +0200

    goto mistake

--- blackjack/Program.cs
+++ blackjack/Program.cs
@@ -8,8 +8,10 @@ new cardProperties() { power=1, name="1"},
 new cardProperties() { power=2, name="2"}
 };
 */
+int tokens = 50;
 Random rndDealer = new Random();
 var Dealer = rndDealer.Next(12, 22);
+Console.WriteLine($"You currenty have {tokens} tokens");
 Console.WriteLine($"Dealer has {Dealer}");
 if (Dealer > 21) { Console.Write(" Dealer has busted, you won."); }
 
@@ -28,41 +30,41 @@ name[1] = "3";
 name[0] = "2";
 ;
 //List<cards> ListofCards = new List<cards>();
-int totalValue = 0;
 Random rnd = new Random();
-
+Restart:
+int totalValue = 0;
 while (true)
-{
-    var Card = name[rnd.Next(0, 11)];
-    if (Card == name[0]) {totalValue += 2;}
-    else if (Card == name[0]) {totalValue += 3;}
-    else if (Card == name[2]) { totalValue += 4; }
-    else if (Card == name[3]) { totalValue += 5; }
-    else if (Card == name[4]) { totalValue += 6; }
-    else if (Card == name[5]) { totalValue += 7; }
-    else if (Card == name[6]) { totalValue += 8; }
-    else if (Card == name[7]) { totalValue += 9; }
-    else if (Card == name[8]) { totalValue += 10; }
-    else if (Card == name[9]) { totalValue += 10; }
-    else if (Card == name[10]) { totalValue += 10; }
-    else if (Card == name[11]) { totalValue += 10; }
-
-    Console.WriteLine(Card);
-    int SumofValues = (totalValue);
-    Console.WriteLine(SumofValues + " value");
-    if (SumofValues > 21) { Console.WriteLine("It is a bust"); break; }
-    Console.WriteLine("do you want to hit(1) or stand(2)?");
-    int choice=Int32.Parse(Console.ReadLine());
-    if (choice == 2)
     {
-        if (SumofValues > Dealer) { Console.WriteLine("You won!"); }
-        else if (SumofValues < Dealer) { Console.WriteLine("You lost!"); }
-        else if(SumofValues == Dealer) { Console.WriteLine("It is a push, no one won,"); }
-            
-            break; }  
-    
+        var Card = name[rnd.Next(0, 11)];
+        if (Card == name[0]) { totalValue += 2; }
+        else if (Card == name[1]) { totalValue += 3; }
+        else if (Card == name[2]) { totalValue += 4; }
+        else if (Card == name[3]) { totalValue += 5; }
+        else if (Card == name[4]) { totalValue += 6; }
+        else if (Card == name[5]) { totalValue += 7; }
+        else if (Card == name[6]) { totalValue += 8; }
+        else if (Card == name[7]) { totalValue += 9; }
+        else if (Card == name[8]) { totalValue += 10; }
+        else if (Card == name[9]) { totalValue += 10; }
+        else if (Card == name[10]) { totalValue += 10; }
+        else if (Card == name[11]) { totalValue += 10; }
+        Console.WriteLine(Card);
+        int SumofValues = (totalValue);
+        Console.WriteLine(SumofValues + " value");
+        if (SumofValues > 21) { Console.WriteLine("It is a bust"); goto Restart; }
+        Console.WriteLine("do you want to hit(1) or stand(2)?");
+        int choice = Int32.Parse(Console.ReadLine());
+        if (choice == 2)
+        {
+            if (SumofValues > Dealer) { Console.WriteLine("You won!"); tokens += 25; Console.WriteLine($"You currenty have {tokens} tokens"); goto Restart; }
+            else if (SumofValues < Dealer) { Console.WriteLine("You lost!"); tokens -= 25; Console.WriteLine($"You currenty have {tokens} tokens"); goto Restart; }
+            else if (SumofValues == Dealer) { Console.WriteLine("It is a push, no one won,"); Console.WriteLine($"You currenty have {tokens} tokens"); goto Restart; }
 
-}
+
+        }
+
+
+    }
 
 
 /*
